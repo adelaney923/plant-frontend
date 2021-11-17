7076cca71aee37750f0a08a48249469a257e1aad
“Alex <alexandriadelaney923@gmail.com>
2021-11-17 11:50:43 -0800

    Set up initial routes/links

---
+++ src/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>My Plants</h1>;
+};
+
+export default Home;
---
+++ src/PlantForm.js
@@ -0,0 +1,8 @@
+
+const PlantForm = () => {
+    return (
+        <h1>Add Plants</h1> 
+    )
+}
+
+export default PlantForm
